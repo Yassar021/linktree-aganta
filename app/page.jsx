a3d2052ce8b7e3789b12c95ad916561550077f38
Yassar021 <yassar.tkj114@gmail.com>
2023-07-05 20:45:14 +0800

    init app

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Box, Container } from "@chakra-ui/react"
+
+const Home = () => {
+    return(
+      <Container  bgImage={'/bg.png'} bgPosition={'center'} bgRepeat={'no-repeat'} bgSize={'cover'}  width={'100%'} height={'100vh'} maxW={'lg'}>
+        <Box></Box>
+      </Container>
+    )
+}
+
+export default Home
